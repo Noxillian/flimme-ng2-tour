72049223af51bf199453174f22d0f5c76f7fdb79
Dustin Leese <dleese@mevango.com>
2017-03-17 13:54:18 +0100

    Added: alpha highlight feature The native element of the current step gets highlighted while the step is active.

--- src/lib/plugin/ng-bootstrap/tour-anchor.directive.ts
+++ src/lib/plugin/ng-bootstrap/tour-anchor.directive.ts
@@ -1,5 +1,5 @@
 import { TourStepTemplateService } from './tour-step-template.service';
-import { TourAnchorDirective } from '../../tour-anchor.directive';
+import { TourAnchorDirective } from '../../tour-anchor.directive';      //Nachfragen warum selbst import
 import { IStepOption, TourService } from '../../tour.service';
 import {
   ComponentFactoryResolver,
@@ -24,6 +24,7 @@ import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed';
 export class TourAnchorNgBootstrapDirective extends NgbPopover implements OnInit, OnDestroy, TourAnchorDirective {
   @Input() public tourAnchor: string;
   private element: ElementRef;
+  private oldstyle: any;
 
   constructor(
     private tourService: TourService, private tourStepTemplate: TourStepTemplateService, _elementRef: ElementRef, _renderer: Renderer,
@@ -32,6 +33,7 @@ export class TourAnchorNgBootstrapDirective extends NgbPopover implements OnInit
   ) {
     super(_elementRef, _renderer, injector, componentFactoryResolver, viewContainerRef, config, ngZone);
     this.element = _elementRef;
+    this.oldstyle = this.element.nativeElement.style;
   }
 
   public ngOnInit(): void {
@@ -43,7 +45,6 @@ export class TourAnchorNgBootstrapDirective extends NgbPopover implements OnInit
   }
 
   public showTourStep(step: IStepOption): void {
-    console.log(step);
     this.ngbPopover = this.tourStepTemplate.template;
     this.popoverTitle = step.title;
     switch (step.placement) {
@@ -68,9 +69,43 @@ export class TourAnchorNgBootstrapDirective extends NgbPopover implements OnInit
     if (!step.preventScrolling) {
       scrollIntoViewIfNeeded(this.element.nativeElement, true);
     }
+
+    /**
+     * Element Changing test
+     */
+
+    //console.log("test: " + this.element.nativeElement.style.backgroundColor);
+    this.element.nativeElement.style.boxShadow = "2px 2px 10px 10px pink";
+    // box-shadow: 0px 0px 5px gray;
+    // document.getElementById("body").style.backgroundColor = 'black';
+    // document.body.style.backgroundColor = 'black';
+    // let test = this.element.nativeElement.style;
+
+
+    // var div = document.createElement("div");
+    // div.style.width = "100px";
+    // div.style.height = "100px";
+    // div.style.background = "red";
+    // div.style.color = "white";
+    // div.innerHTML = "Hello";
+    //
+    // document.getElementById("main").appendChild(div);
+
+    // console.log("t01_step: ", step);
+    // console.log("t01_anchor: " + this.tourAnchor);
+    // console.log("t02_nativeElement: ", this.element.nativeElement);
+    // let elem: HTMLElement = document.getElementById("test");
+    // if (elem === null){
+    //   console.log("t01_message: no element?");
+    // }else{
+    //   console.log("t01_innerHtml: ", elem.innerHTML);
+    //   console.log("t01_classname: " + elem.className);
+    // }
+
   }
 
   public hideTourStep(): void {
+    this.element.nativeElement.style = this.oldstyle;
     this.close();
   }
 }
