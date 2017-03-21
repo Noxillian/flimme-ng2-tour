ad97a7243ff69bc919fbe02cc8e4a2f74f27d0e7
Dustin Leese <dleese@mevango.com>
2017-03-21 14:44:20 +0100

    test fix 3

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
-export { TourNgBootstrapModule } from './src/lib/plugin/ng-bootstrap/ng-bootstrap.module';
-export { TourModule, TourService, TourConsoleModule } from './src/lib/tour.module';
+// export { TourNgBootstrapModule } from './src/lib/plugin/ng-bootstrap/ng-bootstrap.module';
+// export { TourModule, TourService, TourConsoleModule } from './src/lib/tour.module';
