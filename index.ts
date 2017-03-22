f2cf42597abd8944f4ca5f19fc08a9c5bbc821ee
Dustin Leese <dleese@mevango.com>
2017-03-22 09:26:47 +0100

    index export test 1

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
 // export { TourNgBootstrapModule } from './src/lib/plugin/ng-bootstrap/ng-bootstrap.module';
 // export { TourModule, TourService, TourConsoleModule } from './src/lib/tour.module';
+
+import { NgModule }           from '@angular/core';
+import { BrowserModule }      from '@angular/platform-browser';
+
+import { TourNgBootstrapModule } from './src/lib/plugin/ng-bootstrap/ng-bootstrap.module';
+import { TourModule, TourService, TourConsoleModule } from './src/lib/tour.module';
+
+@NgModule({
+    exports: [
+        TourNgBootstrapModule,
+        TourModule,
+        TourService,
+        TourConsoleModule
+    ],
+    imports:      [
+        BrowserModule
+    ]
+})
+export class tour_AppModule { }
